eb11e5102494d5dce049a54699f51f2f180b7125
TuyenPhamm <phamquangtuyen0704@gmail.com>
2023-03-22 17:56:47 +0700

    commit code

--- src/App.js
+++ src/App.js
@@ -2,23 +2,35 @@ import Colors from "./Color";
 import lstProducts from "./store";
 
 function App() {
-  return (
-    <div
-      className="App"
-      style={{ display: "flex", justifyContent: "space-around" }}
-    >
-      {lstProducts.map((product) => (
-        <div key={product.ProductName}>
-          <p>Hãng: {product.BrandCode}</p>
-          <p>Danh mục: {product.CateProCode}</p>
-          <p>Tên code: {product.ProductCode}</p>
-          <p>Tên sản phẩm: {product.ProductName}</p>
-          <p>Giá: {product.Price}</p>
-          <Colors data={product.Colors} />
-        </div>
-      ))}
-    </div>
-  );
+  var errmess = "Không có dữ liệu";
+  if (lstProducts.length > 0) {
+    return (
+      <div
+        className="App"
+        style={{ display: "flex", justifyContent: "space-around" }}
+      >
+        {lstProducts.map(product => (
+          <div
+            key={product.ProductName}>
+            <p>Hãng: {product.BrandCode}</p>
+            <p>Danh mục: {product.CateProCode}</p>
+            <p>Tên code: {product.ProductCode}</p>
+            <p>Tên sản phẩm: {product.ProductName}</p>
+            <p>Giá: {product.Price}</p>
+            <Colors data={product.Colors} />
+          </div>))}
+      </div>
+    )
+  } else {
+    return (
+      <div
+        className="App"
+        style={{ display: "flex", justifyContent: "space-around" }}
+      >
+        {errmess}
+      </div>
+    );
+  }
 }
 
 export default App;
