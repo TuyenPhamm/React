9434207ca75a4c27574f27b7450ccb1da34dd021
TuyenPhamm <phamquangtuyen0704@gmail.com>
2023-03-24 20:18:58 +0700

    pushcode

--- src/App.js
+++ src/App.js
@@ -3,33 +3,27 @@ import lstProducts from "./store";
 
 function App() {
   var errmess = "Không có dữ liệu";
-  if (lstProducts.length > 0) {
-    return (
-      <div
-        className="App"
-        style={{ display: "flex", justifyContent: "space-around" }}
-      >
-        {lstProducts.map(product => (
-          <div
-            key={product.ProductName}>
-            <p>Hãng: {product.BrandCode}</p>
-            <p>Danh mục: {product.CateProCode}</p>
-            <p>Tên code: {product.ProductCode}</p>
-            <p>Tên sản phẩm: {product.ProductName}</p>
-            <p>Giá: {product.Price}</p>
-            <Colors data={product.Colors} />
-          </div>))}
-      </div>
-    )
-  } else {
-    return (
-      <div
-        className="App"
-        style={{ display: "flex", justifyContent: "space-around" }}
-      >
-        {errmess}
-      </div>
-    );
+  let newarr=[];
+  newarr = lstProducts.slice(1,3)
+  console.log(newarr)
+  if (newarr.length > 0) {
+     return (
+        <div
+          className="App"
+          style={{ display: "flex", justifyContent: "space-around" }}
+        >
+          {newarr.length > 0 ? newarr.map(product => (
+            <div
+              key={product.ProductName}>
+              <p>Hãng: {product.BrandCode}</p>
+              <p>Danh mục: {product.CateProCode}</p>
+              <p>Tên code: {product.ProductCode}</p>
+              <p>Tên sản phẩm: {product.ProductName}</p>
+              <p>Giá: {product.Price}</p>
+              <Colors data={product.Colors} />
+            </div>)) : errmess }
+        </div>
+      );
   }
 }
 
